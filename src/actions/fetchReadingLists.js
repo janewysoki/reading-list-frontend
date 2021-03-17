9aff26e350df8b604be7e4fb608932eb283ca63c
janewysoki <jane.wysoki@gmail.com>
2021-03-17 19:19:50 -0400

    use thunk to add dispatch to fetch request

--- src/actions/fetchReadingLists.js
+++ src/actions/fetchReadingLists.js
@@ -3,11 +3,15 @@
 //action creator creats action object and that action obkject gets dispatched to our reducer whihc will then return our new version of state based on the action that we sent
 //thunk allows us to call dispatch from inside this action creator which allows us to wait for this fetch request to finish before we dispatch anything to our reducer
 
+//reading_lists below used to say data and could say data
 export function fetchReadingLists() {
    return (dispatch) => {
       fetch('http://localhost:3000/api/v1/reading_lists') 
       .then(response => response.json()) 
-      .then (data => console.log(data)) 
+      .then (reading_lists => dispatch({
+         type: 'FETCH_READING_LISTS',
+         payload: reading_lists
+      })) 
    }
    
 
