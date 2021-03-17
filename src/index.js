4e3b9aac7406d74c435b95c8ed6b80bf20b7fccd
janewysoki <jane.wysoki@gmail.com>
2021-03-17 14:05:45 -0400

    import and set up compose

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createstore, applyMiddleware } from 'redux'; //need to set up store
+import { createstore, applyMiddleware, compose } from 'redux'; //need to set up store; compose combines different middlewares into 1 so we only have to pass one middleware into createStore
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux'; //this wraps our app and gives it access to store
 import App from './App';
 
 
-//set up store means we need to import
+let reducer = ''
+
+//this is basically saying compose enhances equals these dev tools or equals compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
+
+
+//below we wrap App with Provider so that the store is global to any component we create?
+//any child of our App component will have access to the store that we have to pass into provider
+ReactDOM.render(
+<Provider >
+  <App />
+</Provider>
+, 
+
+document.getElementById('root'));
