fb241a30c0d85d48eafc795ef54feb668deaf026
janewysoki <jane.wysoki@gmail.com>
2021-03-17 20:53:23 -0400

    add local state to ReadingListInput and add value to inputs

--- src/components/ReadingListInput.js
+++ src/components/ReadingListInput.js
@@ -2,17 +2,19 @@ import React from 'react'
 
 //this is a class component because it's a form which has local state values
 class ReadingListInput extends React.Component {
+    //this is local state not redux store
+    state = {name: '', subject: '', description: ''}
 
     render() {
         return(
             <div>
                 <form>
                     <label>Reading List Name: </label>
-                    <input type='text' placeholder='Name' /> <br/>
+                    <input type='text' placeholder='Name' value={this.state.name}/> <br/>
                     <label>Subject:</label>
-                    <input type='text' placeholder='Subject' /> <br/>
+                    <input type='text' placeholder='Subject' value={this.state.subject}/> <br/>
                     <label>Description:</label>
-                    <input type='text' placeholder='Description' />
+                    <input type='text' placeholder='Description' value={this.state.description}/>
                 </form>
             </div>
         )
